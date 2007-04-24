f103b3272cfabbbb26661eae354c06c18c171235
Charles Stephen Kerr <csk@src.gnome.org>
2007-04-24 00:07:21 +0000

    Bug 432387 – [PATCH] 0.128 build failure on FreeBSD/amd64.
    svn path=/trunk/; revision=247

--- pan/data/article.cc
+++ pan/data/article.cc
@@ -153,7 +153,8 @@ Article :: Part :: set_message_id (const Quark& key_mid, const StringView& mid)
 
   kc = &key.back();
   mc = &mid.back();
-  ke = key.str + 1 + std::max (int(b), (int)minlen - (int)UCHAR_MAX);
+  ke = std::max (key.str + b + 1,
+                 key.str + key.len - UCHAR_MAX);
   while (kc>ke)
     if (*kc-- != *mc--) { ++kc; break; }
   const size_t e (&key.back() - kc);
