be6ab3c2320e63e866b7bcff26f9cd781e3fd32d
Charles Stephen Kerr <csk@src.gnome.org>
2007-05-19 20:55:05 +0000

    netbsd compile fix
    svn path=/trunk/; revision=290

--- pan/data/parts.cc
+++ pan/data/parts.cc
@@ -80,7 +80,7 @@ namespace
     register const char *k, *m;
     const StringView& key (key_mid.to_view());
 
-    const int bmax = std::min ((int)std::min (key.len, mid.len), UCHAR_MAX);
+    const int bmax = std::min ((int)std::min (key.len, mid.len), (int)UCHAR_MAX);
     k = &key.front();
     m = &mid.front();
     for (; b!=bmax; ++b)
