b11cee4b7cf4d6b9a3cc54b975bb199d7c3a38df
Heinrich Müller <sphemuel@stud.informatik.uni-erlangen.de>
2011-06-16 10:21:28 +0200

    save fix

--- pan/gui/post-ui.cc
+++ pan/gui/post-ui.cc
@@ -750,9 +750,9 @@ PostUI :: send_and_save_now ()
     else
       return;
 
-    if (!update_queue_save_file ()) return;
-
   }
+
+   if (!update_queue_save_file ()) return;
 
   GMimeMessage * message (new_message_from_ui (POSTING));
   if (!maybe_post_message (message))
