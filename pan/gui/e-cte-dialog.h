0dbceb32f5df94fdf66eb493336d2e72e905fe74
Heinrich Müller <henmull@src.gnome.org>
2011-12-16 23:14:17 +0100

    forgot this

---
+++ pan/gui/e-cte-dialog.h
@@ -0,0 +1,14 @@
+#ifndef E_CTE_DIALOG_H
+#define E_CTE_DIALOG_H
+
+#include <config.h>
+#include <gmime/gmime.h>
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+GMimeContentEncoding e_cte_dialog (const char *title, const char *prompt, GMimeContentEncoding now, GtkWindow *parent);
+
+G_END_DECLS
+
+#endif
