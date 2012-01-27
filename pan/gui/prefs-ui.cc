0c050096089f3d74c1cd6dd057d10f4d2cc58bb6
Heinrich Müller <henmull@src.gnome.org>
2012-01-27 11:49:57 +0100

    quick gtk fix

--- pan/gui/prefs-ui.cc
+++ pan/gui/prefs-ui.cc
@@ -126,11 +126,19 @@ PrefsDialog :: edit_shortkey (gpointer user_data)
       _("Press the combination of the keys\nyou want to use for \"%s\"."), data->value.c_str());
   label = gtk_label_new(str);
   gtk_misc_set_padding(GTK_MISC(label), 5, 10);
-  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog)->vbox), label);
+
+  gtk_box_pack_start (GTK_BOX (gtk_dialog_get_content_area (GTK_DIALOG (dialog))),
+		      label,
+		      FALSE, FALSE,
+		      2);
 
   data->label = gtk_label_new("");
   gtk_misc_set_padding(GTK_MISC(data->label), 5, 10);
-  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog)->vbox), data->label);
+
+  gtk_box_pack_start (GTK_BOX (gtk_dialog_get_content_area (GTK_DIALOG (dialog))),
+		      data->label,
+		      FALSE, FALSE,
+		      2);
 
   g_signal_connect(dialog, "key-press-event",
             G_CALLBACK(hotkey_key_press_cb), user_data);
