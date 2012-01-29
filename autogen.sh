82611380bb239145b7c82a49bae48c0ad33968a7
Petr Kovar <pkovar@redhat.com>
2012-01-29 02:49:10 +0100

    Update autotools config and docs

--- autogen.sh
+++ autogen.sh
@@ -5,18 +5,19 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="Pan"
-REQUIRED_AUTOMAKE_VERSION=1.9
 
-(test -f $srcdir/configure.in \
-  && test -d $srcdir/pan) || {
+(test -f $srcdir/Makefile.am && test -d $srcdir/pan) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level pan directory"
+    echo " top-level $PKG_NAME directory"
     exit 1
 }
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
+    echo "You need to install gnome-common module and make"
+    echo "sure the gnome-autogen.sh script is in your \$PATH."
     exit 1
 }
 
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+USE_GNOME2_MACROS=1
+
+. gnome-autogen.sh
