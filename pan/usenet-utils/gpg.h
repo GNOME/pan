ee495f9f1ef7bd4d207c7f2d15e3a6ca1ee01bcf
Heinrich Müller <henmull@src.gnome.org>
2012-03-27 19:24:57 +0200

    removed dereferencing of decrypt result (gpg)

--- pan/usenet-utils/gpg.h
+++ pan/usenet-utils/gpg.h
@@ -82,12 +82,12 @@ namespace pan
     GMimeObject * decrypted;
     GMimeDecryptResult * result;
 
-    GPGDecErr(GPGDecType t) : err(NULL), type(t), decrypted(0), result(g_mime_decrypt_result_new())  {}
+    GPGDecErr(GPGDecType t) : err(NULL), type(t), decrypted(NULL), result(g_mime_decrypt_result_new())  {}
     ~GPGDecErr()
       {
         if (err) g_error_free(err);
         err = NULL;
-        if (decrypted) g_object_unref(decrypted) ;
+//        if (decrypted) g_object_unref(decrypted) ;
         if (result) g_object_unref(result);
       }
       GPGDecErr() : no_sigs(true) {}
