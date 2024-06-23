fdc65a02b6c09b9573af2460ba27997d4b2f0a0c
Dominique Dumont <domi.dumont@free.fr>
2024-06-23 18:14:21 +0200

    group schema: add group.read_ranges

--- pan/data-impl/sql/02-group.sql
+++ pan/data-impl/sql/02-group.sql
@@ -1,6 +1,22 @@
 create table if not exists `group` (
   id integer primary key asc autoincrement,
   name text not null,
+
+  -- From Thomas T: In theory these are denormalisations because if a
+  -- server holds the group it should in general hold all the articles
+  -- in the group. However, we don't fetch information from all
+  -- servers so it's not entirely, and in any case a specific server
+  -- may not have got all the articles yet.
+
+  -- In theory, a group should contain a list of articles, each with a read/not-read status.
+  -- In practice, retrieving these status can be quite long.
+  -- Anyway, pan internal data use these ranges, so I'll stick with them for now. I may
+  -- revisit this and remove this attribute if storing a status per article is viable
+  -- (which may require subtle usage of indexes and views)
+
+  -- stored as a list of ranges
+  read_ranges text,
+
   subscribed boolean check (subscribed in (False, True))
 );
 
