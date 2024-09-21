e9fdd042766988d2dc4500953c961e79cfd6b346
Dominique Dumont <domi.dumont@free.fr>
2024-09-21 16:37:55 +0200

    refactor(data-impl): remove newsrc autosave

--- pan/data-impl/data-impl.cc
+++ pan/data-impl/data-impl.cc
@@ -93,9 +93,7 @@ DataImpl ::DataImpl(StringView const &cache_ext,
   _prefs(prefs),
   _cached_xover_entry(nullptr),
   _article_rules(prefs.get_flag("rules-autocache-mark-read", false),
-                prefs.get_flag("rules-auto-dl-mark-read", false)),
-  newsrc_autosave_id(0),
-  newsrc_autosave_timeout(0)
+                prefs.get_flag("rules-auto-dl-mark-read", false))
 {
   rebuild_backend ();
 }
