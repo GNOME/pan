e6ffac221127d97be9f4fb23b753cfb6f9284f9a
Dominique Dumont <domi.dumont@free.fr>
2024-10-03 17:48:25 +0200

    chore(article): reformated with ccls

--- pan/data/article.h
+++ pan/data/article.h
@@ -21,89 +21,155 @@
 #define __Article_h__
 
 #include <ctime>
-#include <vector>
-#include <pan/general/sorted-vector.h>
-#include <pan/general/quark.h>
 #include <pan/data/parts.h>
 #include <pan/data/xref.h>
+#include <pan/general/quark.h>
+#include <pan/general/sorted-vector.h>
+#include <vector>
 
-namespace pan
+namespace pan {
+/**
+ * A Usenet article, either single-part or multipart.
+ *
+ * To lessen the memory footprint of large binaries groups,
+ * Pan folds multipart posts into a single Article object.
+ * Only minimal information for any one part is kept
+ * (message-id, line count, byte count), and the Article object
+ * holds the rest.
+ *
+ * This is a lossy approach: less-important unique fields,
+ * such as the part's xref and time-posted, are not needed
+ * and so we don't keep them.
+ *
+ * @ingroup data
+ */
+
+// used in both huge model and as tmp data structure for other
+// operation like encode (presumably to send article). Parts are
+// also used this way so this class must stays as is. Reading
+// article may rely on another class, like ArticleInDb and
+// PartsInDb.
+class Article
 {
-  /**
-   * A Usenet article, either single-part or multipart.
-   *
-   * To lessen the memory footprint of large binaries groups,
-   * Pan folds multipart posts into a single Article object.
-   * Only minimal information for any one part is kept
-   * (message-id, line count, byte count), and the Article object
-   * holds the rest.
-   *
-   * This is a lossy approach: less-important unique fields,
-   * such as the part's xref and time-posted, are not needed
-   * and so we don't keep them.
-   *
-   * @ingroup data
-   */
+  public:
+    void set_parts(PartBatch const &b)
+    {
+      parts.set_parts(b);
+    }
 
-  // used in both huge model and as tmp data structure for other
-  // operation like encode (presumably to send article). Parts are
-  // also used this way so this class must stays as is. Reading
-  // article may rely on another class, like ArticleInDb and
-  // PartsInDb.
-  class Article
-  {
-    public:
-      void set_parts (const PartBatch& b) { parts.set_parts(b); }
-      bool add_part (Parts::number_t num, const StringView& mid, Parts::bytes_t bytes) { return parts.add_part(num,mid,bytes,message_id); }
-      void set_part_count (Parts::number_t num) { parts.set_part_count(num); }
-      Parts::number_t get_total_part_count () const { return parts.get_total_part_count(); }
-      Parts::number_t get_found_part_count () const { return parts.get_found_part_count(); }
-      bool get_part_info (Parts::number_t      num,
-                          std::string & mid,
-                          Parts::bytes_t     & bytes) const { return parts.get_part_info(num,mid,bytes,message_id); }
+    bool add_part(Parts::number_t num,
+                  StringView const &mid,
+                  Parts::bytes_t bytes)
+    {
+      return parts.add_part(num, mid, bytes, message_id);
+    }
 
-      typedef Parts::const_iterator part_iterator;
-      part_iterator pbegin() const { return parts.begin(message_id); }
-      part_iterator pend() const { return parts.end(message_id); }
+    void set_part_count(Parts::number_t num)
+    {
+      parts.set_part_count(num);
+    }
 
-      typedef std::vector<Quark> mid_sequence_t;
-      mid_sequence_t get_part_mids () const;
-      enum PartState { SINGLE, INCOMPLETE, COMPLETE };
-      PartState get_part_state () const;
+    Parts::number_t get_total_part_count() const
+    {
+      return parts.get_total_part_count();
+    }
 
-    public:
-      Quark message_id;
-      Quark author;
-      Quark subject;
-      time_t time_posted;
-      unsigned int lines;
-      int score;
-      bool is_binary;
-      bool flag;
-      static bool has_reply_leader (const StringView&);
+    Parts::number_t get_found_part_count() const
+    {
+      return parts.get_found_part_count();
+    }
 
-    public:
-      unsigned int get_crosspost_count () const;
-      unsigned long get_line_count () const { return lines; }
-      bool is_line_count_ge (size_t test) const { return lines >= test; }
-      unsigned long get_byte_count () const;
-      bool is_byte_count_ge (unsigned long test) const;
+    bool get_part_info(Parts::number_t num,
+                       std::string &mid,
+                       Parts::bytes_t &bytes) const
+    {
+      return parts.get_part_info(num, mid, bytes, message_id);
+    }
 
-      Xref xref;
+    typedef Parts::const_iterator part_iterator;
 
-    public:
-      Article (): time_posted(0), lines(0), score(0), is_binary(false), flag(false)  {}
-      void clear ();
+    part_iterator pbegin() const
+    {
+      return parts.begin(message_id);
+    }
 
-      /* Functions to bookmark an article */
-      void toggle_flag() { flag = !flag; }
-      bool get_flag() const { return flag; }
-      void set_flag(bool setme) { flag = setme; }
+    part_iterator pend() const
+    {
+      return parts.end(message_id);
+    }
 
-    private:
-      Parts parts;
+    typedef std::vector<Quark> mid_sequence_t;
+    mid_sequence_t get_part_mids() const;
 
-  };
-}
+    enum PartState
+    {
+      SINGLE,
+      INCOMPLETE,
+      COMPLETE
+    };
+
+    PartState get_part_state() const;
+
+  public:
+    Quark message_id;
+    Quark author;
+    Quark subject;
+    time_t time_posted;
+    unsigned int lines;
+    int score;
+    bool is_binary;
+    bool flag;
+    static bool has_reply_leader(StringView const &);
+
+  public:
+    unsigned int get_crosspost_count() const;
+
+    unsigned long get_line_count() const
+    {
+      return lines;
+    }
+
+    bool is_line_count_ge(size_t test) const
+    {
+      return lines >= test;
+    }
+
+    unsigned long get_byte_count() const;
+    bool is_byte_count_ge(unsigned long test) const;
+
+    Xref xref;
+
+  public:
+    Article() :
+      time_posted(0),
+      lines(0),
+      score(0),
+      is_binary(false),
+      flag(false)
+    {
+    }
+
+    void clear();
+
+    /* Functions to bookmark an article */
+    void toggle_flag()
+    {
+      flag = ! flag;
+    }
+
+    bool get_flag() const
+    {
+      return flag;
+    }
+
+    void set_flag(bool setme)
+    {
+      flag = setme;
+    }
+
+  private:
+    Parts parts;
+};
+} // namespace pan
 
 #endif
