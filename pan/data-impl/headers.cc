434ffb296a5e9dba7b897e7955a3e2bf15cb331c
Dominique Dumont <domi.dumont@free.fr>
2024-10-08 20:09:02 +0200

    refactor(groups.cc): remove _xover_high attribute
    from group object. xover_high is now stored and retrieved direclty to
    and from DB.

--- pan/data-impl/headers.cc
+++ pan/data-impl/headers.cc
@@ -1319,8 +1319,6 @@ bool DataImpl ::save_headers(DataIO &data_io,
 
       store_article_transaction.commit();
     }
-
-    save_group_xovers();
   }
 
   LOG4CXX_DEBUG(logger, "Done saving new articles of groups " << group.c_str()
