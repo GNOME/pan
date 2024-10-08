97e32827ab60951cb4292af0cbdf6930d7049ccd
Dominique Dumont <domi.dumont@free.fr>
2024-10-08 19:58:52 +0200

    chore(sqlite): globally set pragma synchronous = off
    To speed up writing in file DB. This is necessary due to the amount of
    writes performed when loading new articles.
    
    see https://www.sqlite.org/pragma.html#pragma_synchronous

--- pan/data-impl/headers.cc
+++ pan/data-impl/headers.cc
@@ -584,10 +584,6 @@ void DataImpl ::migrate_headers(DataIO const &data_io, Quark const &group)
     values ((select id from article where message_id = ?), ?, ?, ?) on conflict do nothing;
   )SQL");
 
-  // speed insert up -- from minutes to seconds
-  // see https://www.sqlite.org/pragma.html#pragma_synchronous
-  pan_db.exec("pragma synchronous = off");
-
   LOG4CXX_INFO(logger, "Migrating articles of group " << group.c_str()
                << " in DB. Please wait a few minutes.");
 
@@ -867,8 +863,6 @@ void DataImpl ::migrate_headers(DataIO const &data_io, Quark const &group)
     }
   }
   delete in;
-
-  pan_db.exec("pragma synchronous = normal");
 
   // update the group's article count...
   ReadGroup &g(_read_groups[group]);
@@ -1247,10 +1241,6 @@ bool DataImpl ::save_headers(DataIO &data_io,
     values ((select id from article where message_id = ?), ?, ?, ?) on conflict do nothing;
   )SQL");
 
-  // speed insert up -- from minutes to seconds
-  // see https://www.sqlite.org/pragma.html#pragma_synchronous
-  pan_db.exec("pragma synchronous = off");
-
   LOG4CXX_DEBUG(logger, "Saving new articles of groups " << group.c_str()
                 << " in DB...");
 
@@ -1343,7 +1333,6 @@ bool DataImpl ::save_headers(DataIO &data_io,
     save_group_xovers();
   }
 
-  pan_db.exec("pragma synchronous = normal");
   LOG4CXX_DEBUG(logger, "Done saving new articles of groups " << group.c_str()
                 << " in DB...");
 
