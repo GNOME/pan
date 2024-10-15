b03d7e9fe321bd75079013aed7f050e7a359834d
Dominique Dumont <domi.dumont@free.fr>
2024-10-15 18:23:01 +0200

    chore(task-xover.*): re-indented by ccls

--- pan/tasks/task-xover.h
+++ pan/tasks/task-xover.h
@@ -21,75 +21,98 @@
 #define __TASK_XOVER__H__
 
 #include <map>
-#include <vector>
 #include <sstream>
 
-#include <pan/general/compression.h>
 #include <pan/data/data.h>
-#include <pan/tasks/task.h>
+#include <pan/general/compression.h>
 #include <pan/tasks/nntp.h>
-#include <fstream>
-#include <iostream>
+#include <pan/tasks/task.h>
 
-namespace pan
+namespace pan {
+/**
+ * Task for downloading a some or all of a newsgroups' headers
+ * @ingroup tasks
+ */
+class TaskXOver : public Task, private NNTP::Listener
 {
-  /**
-   * Task for downloading a some or all of a newsgroups' headers
-   * @ingroup tasks
-   */
-  class TaskXOver: public Task, private NNTP::Listener
-  {
-    public: // life cycle
-      enum Mode { ALL, NEW, SAMPLE, DAYS };
-      TaskXOver (Data& data, const Quark& group, Mode mode=ALL, unsigned long sample_size=1000);
-      virtual ~TaskXOver ();
+  public: // life cycle
+    enum Mode
+    {
+      ALL,
+      NEW,
+      SAMPLE,
+      DAYS
+    };
 
-    public: // task subclass
-      virtual unsigned long get_bytes_remaining () const override;
+    TaskXOver(Data &data,
+              Quark const &group,
+              Mode mode = ALL,
+              unsigned long sample_size = 1000);
+    virtual ~TaskXOver();
 
-    protected: // task subclass
-      virtual void use_nntp (NNTP * nntp) override;
+  public: // task subclass
+    virtual unsigned long get_bytes_remaining() const override;
 
-    private: // NNTP::Listener
-      virtual void on_nntp_line (NNTP*, const StringView&) override;
-      virtual void on_nntp_done (NNTP*, Health, const StringView&) override;
-      virtual void on_nntp_group (NNTP*, const Quark&, Article_Count, Article_Number, Article_Number) override;
+  protected: // task subclass
+    virtual void use_nntp(NNTP *nntp) override;
 
-      void on_nntp_line_process (NNTP*, const StringView&);
+  private: // NNTP::Listener
+    virtual void on_nntp_line(NNTP *, StringView const &) override;
+    virtual void on_nntp_done(NNTP *, Health, StringView const &) override;
+    virtual void on_nntp_group(NNTP *,
+                               Quark const &,
+                               Article_Count,
+                               Article_Number,
+                               Article_Number) override;
 
-    private: // implementation - minitasks
-      struct MiniTask {
-        enum Type { GROUP, XOVER };
+    void on_nntp_line_process(NNTP *, StringView const &);
+
+  private: // implementation - minitasks
+    struct MiniTask
+    {
+        enum Type
+        {
+          GROUP,
+          XOVER
+        };
+
         Type _type;
         Article_Number _low, _high;
-        MiniTask (Type type, Article_Number low=static_cast<Article_Number>(0ul), Article_Number high=static_cast<Article_Number>(0ul)):
-          _type(type), _low(low), _high(high) {}
-      };
-      typedef std::deque<MiniTask> MiniTasks_t;
-      typedef std::map<Quark,MiniTasks_t> server_to_minitasks_t;
-      server_to_minitasks_t _server_to_minitasks;
 
-    private: // implementation
-      Data& _data;
-      const Quark _group;
-      std::string _short_group_name;
-      Mode _mode;
-      uint64_t _sample_size;
-      time_t _days_cutoff;
-      bool _group_xover_is_reffed;
-      typedef std::map<Quark,Article_Number> server_to_high_t;
-      server_to_high_t _high;
-      void update_work (bool subtract_one_from_nntp_count=false);
-      std::set<Quark> _servers_that_got_xover_minitasks;
-      std::map<NNTP*, Article_Number> _last_xover_number;
-      unsigned long _bytes_so_far;
-      unsigned long _parts_so_far;
-      unsigned long _articles_so_far;
-      unsigned long _total_minitasks;
+        MiniTask(Type type,
+                 Article_Number low = static_cast<Article_Number>(0ul),
+                 Article_Number high = static_cast<Article_Number>(0ul)) :
+          _type(type),
+          _low(low),
+          _high(high)
+        {
+        }
+    };
 
-      std::map<int, std::stringstream*> _streams;
+    typedef std::deque<MiniTask> MiniTasks_t;
+    typedef std::map<Quark, MiniTasks_t> server_to_minitasks_t;
+    server_to_minitasks_t _server_to_minitasks;
 
-  };
-}
+  private: // implementation
+    Data &_data;
+    Quark const _group;
+    std::string _short_group_name;
+    Mode _mode;
+    uint64_t _sample_size;
+    time_t _days_cutoff;
+    bool _group_xover_is_reffed;
+    typedef std::map<Quark, Article_Number> server_to_high_t;
+    server_to_high_t _high;
+    void update_work(bool subtract_one_from_nntp_count = false);
+    std::set<Quark> _servers_that_got_xover_minitasks;
+    std::map<NNTP *, Article_Number> _last_xover_number;
+    unsigned long _bytes_so_far;
+    unsigned long _parts_so_far;
+    unsigned long _articles_so_far;
+    unsigned long _total_minitasks;
+
+    std::map<int, std::stringstream *> _streams;
+};
+} // namespace pan
 
 #endif
