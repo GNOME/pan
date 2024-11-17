f64aa3a08330d21bd64c72c25612222c82afb0f4
Dominique Dumont <domi.dumont@free.fr>
2024-11-17 18:27:12 +0100

    reformat task.cc

--- pan/tasks/task.cc
+++ pan/tasks/task.cc
@@ -17,19 +17,21 @@
  *
  */
 
+#include "task.h"
 #include <config.h>
 #include <pan/general/debug.h>
 #include <pan/general/messages.h>
-#include "task.h"
 
 using namespace pan;
 
-Task :: Task (const Quark& type, const StringView& description): Progress(description), _type(type)
+Task ::Task(Quark const &type, StringView const &description) :
+  Progress(description),
+  _type(type)
 {
-  verbose (_description);
+  verbose(_description);
 }
 
-Task :: ~Task ()
+Task ::~Task()
 {
 }
 
@@ -37,82 +39,77 @@ Task :: ~Task ()
 ****  Socket Handling
 ***/
 
-void
-Task :: give_nntp (NNTP::Source * source, NNTP* nntp)
+void Task ::give_nntp(NNTP::Source *source, NNTP *nntp)
 {
   _nntp_to_source[nntp] = source;
-  pan_debug ("gave nntp " << nntp->_server << " (" << nntp << ") to task " << this << ", which now has " << _nntp_to_source.size() << " nntps");
-  use_nntp (nntp);
+  pan_debug("gave nntp " << nntp->_server << " (" << nntp << ") to task "
+                         << this << ", which now has " << _nntp_to_source.size()
+                         << " nntps");
+  use_nntp(nntp);
 }
 
-void
-Task :: check_in (NNTP * nntp, Health health)
+void Task ::check_in(NNTP *nntp, Health health)
 {
-   pan_debug ("task " << this << " returning nntp " << nntp);
+  pan_debug("task " << this << " returning nntp " << nntp);
 
-   nntp_to_source_t::iterator it = _nntp_to_source.find (nntp);
-   if (it != _nntp_to_source.end())
-   {
-      NNTP::Source * source = it->second;
-      _nntp_to_source.erase (nntp);
-      pan_debug ("returned nntp " << nntp << " OK; task " << this << " now has " << _nntp_to_source.size() << " nntps");
+  nntp_to_source_t::iterator it = _nntp_to_source.find(nntp);
+  if (it != _nntp_to_source.end())
+  {
+    NNTP::Source *source = it->second;
+    _nntp_to_source.erase(nntp);
+    pan_debug("returned nntp " << nntp << " OK; task " << this << " now has "
+                               << _nntp_to_source.size() << " nntps");
 
-      source->check_in (nntp, health);
-   }
+    source->check_in(nntp, health);
+  }
 }
 
 /***
 ****
 ***/
 
-void
-Task :: give_decoder (DecoderSource* s, Decoder* d)
+void Task ::give_decoder(DecoderSource *s, Decoder *d)
 {
   _decoder_to_source[d] = s;
-  use_decoder (d);
+  use_decoder(d);
 }
 
-void
-Task :: give_encoder (EncoderSource* s, Encoder* d)
+void Task ::give_encoder(EncoderSource *s, Encoder *d)
 {
   _encoder_to_source[d] = s;
-  use_encoder (d);
+  use_encoder(d);
 }
 
-void
-Task :: use_decoder (Decoder * d UNUSED)
+void Task ::use_decoder(Decoder *d UNUSED)
 {
   assert(0 && "oops, child class task.cc called!\n");
 }
 
-void
-Task :: use_encoder (Encoder * d UNUSED)
+void Task ::use_encoder(Encoder *d UNUSED)
 {
   assert(0 && "oops, child class task.cc called!\n");
 }
 
-void
-Task :: check_in (Decoder * d)
+void Task ::check_in(Decoder *d)
 {
-  decoder_to_source_t::iterator it (_decoder_to_source.find (d));
+  decoder_to_source_t::iterator it(_decoder_to_source.find(d));
 
   if (it != _decoder_to_source.end())
   {
-    DecoderSource * s (it->second);
-    _decoder_to_source.erase (d);
-    s->check_in (d, this);
+    DecoderSource *s(it->second);
+    _decoder_to_source.erase(d);
+    s->check_in(d, this);
   }
 }
 
-void
-Task :: check_in (Encoder * d)
+void Task ::check_in(Encoder *d)
 {
-  encoder_to_source_t::iterator it (_encoder_to_source.find (d));
+  encoder_to_source_t::iterator it(_encoder_to_source.find(d));
 
   if (it != _encoder_to_source.end())
   {
-    EncoderSource * s (it->second);
-    _encoder_to_source.erase (d);
-    s->check_in (d, this);
+    EncoderSource *s(it->second);
+    _encoder_to_source.erase(d);
+    s->check_in(d, this);
   }
 }
