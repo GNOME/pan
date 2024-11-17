71a5f6962da9d40275261f5b611e7b4a8ad37379
Dominique Dumont <domi.dumont@free.fr>
2024-11-17 18:50:13 +0100

    task: add logger

--- pan/general/log4cxx.cc
+++ pan/general/log4cxx.cc
@@ -60,6 +60,7 @@ log4cxx::LoggerPtr getLogger(std::string const &name)
           outfile << "log4j.logger.nzb=WARN, A1" << std::endl;
           outfile << "log4j.logger.queue=WARN, A1" << std::endl;
           outfile << "log4j.logger.server=WARN, A1" << std::endl;
+          outfile << "log4j.logger.task=WARN, A1" << std::endl;
           outfile << "log4j.logger.task-article=WARN, A1" << std::endl;
           outfile << "log4j.logger.task-xover=WARN, A1" << std::endl;
           outfile << "log4j.logger.xover=WARN, A1" << std::endl;
