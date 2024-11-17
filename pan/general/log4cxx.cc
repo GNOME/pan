9ef93a47372ff5751733046973fc73eb43ecbf22
Dominique Dumont <domi.dumont@free.fr>
2024-11-17 18:48:15 +0100

    chore(queue): add log4cxx

--- pan/general/log4cxx.cc
+++ pan/general/log4cxx.cc
@@ -56,6 +56,7 @@ log4cxx::LoggerPtr getLogger(std::string const &name)
           outfile << "log4j.logger.header=WARN, A1" << std::endl;
           outfile << "log4j.logger.header-tree=WARN, A1" << std::endl;
           outfile << "log4j.logger.nzb=WARN, A1" << std::endl;
+          outfile << "log4j.logger.queue=WARN, A1" << std::endl;
           outfile << "log4j.logger.server=WARN, A1" << std::endl;
           outfile << "log4j.logger.task-article=WARN, A1" << std::endl;
           outfile << "log4j.logger.task-xover=WARN, A1" << std::endl;
