e3585091cb0a473235033f0f143ea8e94ce05c34
Dominique Dumont <domi.dumont@free.fr>
2024-11-30 18:54:57 +0100

    load_headers_from_db: drop unread article count
    not used anymore.

--- pan/data-impl/headers.cc
+++ pan/data-impl/headers.cc
@@ -933,7 +933,6 @@ void DataImpl ::load_headers_from_db(Quark const &group) {
   assert(h != nullptr);
 
   Article_Count article_count(0);
-  Article_Count unread_count(0);
   StringView line;
   quarks_t servers;
 
@@ -1019,9 +1018,6 @@ void DataImpl ::load_headers_from_db(Quark const &group) {
     // build article tree in memory.
     load_article(group, &a, references);
     ++article_count;
-    if (! is_read(&a)) {
-      ++unread_count;
-    }
   }
 
   fire_group_counts(group);
