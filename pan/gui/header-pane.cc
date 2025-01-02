86c938dec1e9742c50f37bf09794836dd28a7723
Dominique Dumont <domi.dumont@free.fr>
2025-01-02 16:19:35 +0100

    header-pane row: no longer store action in Row
    Looks like action attribute was no longer read from

--- pan/gui/header-pane.cc
+++ pan/gui/header-pane.cc
@@ -2886,13 +2886,7 @@ void HeaderPane ::rebuild_article_action(Quark const &message_id)
   Row *row(get_row(message_id));
   if (row)
   {
-    const Article* a = row->article;
-    bool flag(false);
-    if (a) {
-      flag = a->get_flag();
-    }
-
-    row->action = get_article_action(flag, message_id);
+    // still needed to update action column right after change.
     _tree_store->row_changed(row);
   }
 }
@@ -2952,7 +2946,7 @@ struct HeaderPane::SimilarWalk : public PanTreeStore::WalkFunctor
 {
   private:
     GtkTreeSelection *selection;
-    const Article source;
+    Article const source;
 
   public:
     virtual ~SimilarWalk()
