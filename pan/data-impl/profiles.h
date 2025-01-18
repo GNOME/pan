94b934a1948ef317517bc2b4cecdc44ba5ec2ddc
Dominique Dumont <domi.dumont@free.fr>
2025-01-18 18:40:20 +0100

    profiles.*: reformated with ccls

--- pan/data-impl/profiles.h
+++ pan/data-impl/profiles.h
@@ -20,51 +20,51 @@
 #ifndef __Profiles_h__
 #define __Profiles_h__
 
+#include <iosfwd>
 #include <map>
+#include <pan/data/data.h>
+#include <pan/general/string-view.h>
 #include <set>
 #include <string>
-#include <vector>
-#include <iosfwd>
-#include <pan/general/string-view.h>
-#include <pan/data/data.h>
 
-namespace pan
+namespace pan {
+class DataIO;
+
+/**
+ * This private class should only be used by classes in the same module.
+ *
+ * It's an implementation of the Profiles class.
+ */
+class ProfilesImpl : public virtual Profiles
 {
-  class DataIO;
+  public:
+    ProfilesImpl(DataIO &io);
+    virtual ~ProfilesImpl();
 
-  /**
-   * This private class should only be used by classes in the same module.
-   *
-   * It's an implementation of the Profiles class.
-   */
-  class ProfilesImpl: public virtual Profiles
-  {
-    public:
-      ProfilesImpl (DataIO& io);
-      virtual ~ProfilesImpl ();
+  public:
+    std::set<std::string> get_profile_names() const override;
+    bool has_profiles() const override;
+    bool has_from_header(StringView const &from) const override;
+    bool get_profile(const std::string &profile_name,
+                     Profile &setme) const override;
 
-    public:
-      std::set<std::string> get_profile_names () const override;
-      bool has_profiles () const override;
-      bool has_from_header (const StringView& from) const override;
-      bool get_profile (const std::string& profile_name, Profile& setme) const override;
+  public:
+    void delete_profile(const std::string &profile_name) override;
+    void add_profile(const std::string &profile_name,
+                     const Profile &profile) override;
 
-    public:
-      void delete_profile (const std::string& profile_name) override;
-      void add_profile (const std::string& profile_name, const Profile& profile) override;
+  private:
+    void clear();
+    void load(const StringView &filename);
+    void serialize(std::ostream &) const;
+    void save() const;
 
-    private:
-      void clear ();
-      void load (const StringView& filename);
-      void serialize (std::ostream&) const;
-      void save () const;
-
-    private:
-      typedef std::map<std::string,Profile> profiles_t;
-      profiles_t profiles;
-      std::string active_profile;
-      DataIO& _data_io;
-  };
-}
+  private:
+    typedef std::map<std::string, Profile> profiles_t;
+    profiles_t profiles;
+    std::string active_profile;
+    DataIO &_data_io;
+};
+} // namespace pan
 
 #endif
