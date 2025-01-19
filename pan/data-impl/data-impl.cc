330aecf2665973c0f99ea071981669409dc72cc2
Dominique Dumont <domi.dumont@free.fr>
2025-01-19 16:36:16 +0100

    sql: add posting-profile tables

--- pan/data-impl/data-impl.cc
+++ pan/data-impl/data-impl.cc
@@ -124,8 +124,12 @@ void load_db_schema_file(SQLiteDb &pan_db, char const *file)
   g_free(contents);
 }
 
-void load_db_schema(SQLiteDb &pan_db) {
-  std::vector<std::string> sql_files { "01-server.sql", "02-group.sql", "03-article.sql" };
+void load_db_schema(SQLiteDb &pan_db)
+{
+  std::vector<std::string> sql_files{"01-server.sql",
+                                     "02-group.sql",
+                                     "03-article.sql",
+                                     "04-posting-profile.sql"};
   for (int i = 0; i < sql_files.size(); i++) {
     load_db_schema_file(pan_db, sql_files[i].c_str());
   }
