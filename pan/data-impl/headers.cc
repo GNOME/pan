215ae1dd9b7fce47978c358abdcdedb2c1b3aad9
Dominique Dumont <domi.dumont@free.fr>
2025-02-18 18:28:54 +0100

    add comments

--- pan/data-impl/headers.cc
+++ pan/data-impl/headers.cc
@@ -1210,6 +1210,7 @@ void DataImpl ::get_article_scores(Quark const &group,
   _article_filter.get_article_scores(*this, sections, group, article, setme);
 }
 
+// Compute the score of a list of articles
 void DataImpl ::rescore_articles(Quark const &group, const quarks_t mids)
 {
   ArticleFilter::sections_t sections;
@@ -1225,6 +1226,7 @@ void DataImpl ::rescore_articles(Quark const &group, const quarks_t mids)
   }
 }
 
+// compute the score of all articles of a group
 void DataImpl ::rescore_group_articles(Quark const &group)
 {
   ArticleFilter::sections_t sections;
