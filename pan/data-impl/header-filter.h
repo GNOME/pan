e6d5f8514c012f2a35fa84a92c9acb4f8543a4a4
Dominique Dumont <domi.dumont@free.fr>
2025-03-28 18:53:59 +0100

    refactor(header-filter): extract SqlCond in sqlcond.h

--- pan/data-impl/header-filter.h
+++ pan/data-impl/header-filter.h
@@ -4,6 +4,7 @@
 #include <SQLiteCpp/Statement.h>
 #include <pan/data/article.h>
 #include <pan/data/data.h>
+#include <pan/data/sqlcond.h>
 #include <pan/general/quark.h>
 #include <pan/usenet-utils/filter-info.h>
 #include <pan/usenet-utils/rules-info.h>
@@ -11,63 +12,6 @@
 #include <string>
 
 namespace pan {
-
-class SqlCond
-{
-  public:
-    enum cond_type
-    {
-      NO_PARAM,
-      STR_PARAM,
-      BOOL_PARAM,
-      INT_PARAM
-    };
-
-    std::string join;
-    std::string where;
-    std::string str_param;
-    int int_param;
-    bool bool_param;
-    cond_type type;
-
-    SqlCond(std::string const where, std::string const _p) :
-      where(where),
-      str_param(_p),
-      type(STR_PARAM)
-    {
-    }
-
-    SqlCond(std::string const where, char const *_p) :
-      where(where),
-      str_param(std::string(_p)),
-      type(STR_PARAM)
-    {
-    }
-
-    SqlCond(std::string const where, long const _p) :
-      where(where),
-      int_param(_p),
-      type(INT_PARAM)
-    {
-    }
-
-    SqlCond(std::string const where, bool const _p) :
-      where(where),
-      bool_param(_p),
-      type(BOOL_PARAM)
-    {
-    }
-
-    SqlCond(std::string const where) :
-      where(where),
-      type(NO_PARAM)
-    {
-    }
-
-    SqlCond()
-    {
-    }
-};
 
 class HeaderFilter
 {
