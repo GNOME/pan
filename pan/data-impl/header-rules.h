898cdb63ef6283f4cc12d9727a167dc465bdcf71
Dominique Dumont <domi.dumont@free.fr>
2025-04-06 19:38:46 +0200

    feat(): add header-rules skeleton

---
+++ pan/data-impl/header-rules.h
@@ -0,0 +1,27 @@
+#ifndef HEADER_RULES_H
+#define HEADER_RULES_H
+
+#include <SQLiteCpp/Statement.h>
+#include <pan/data/article.h>
+#include <pan/data/data.h>
+#include <pan/data/sqlcond.h>
+#include <pan/general/quark.h>
+#include <pan/usenet-utils/filter-info.h>
+#include <pan/usenet-utils/rules-info.h>
+#include <pan/usenet-utils/scorefile.h>
+
+namespace pan {
+
+    // used to perform actions (like cache, download, delete) on
+    // article depending on their score (set in pref->action menu)
+class HeaderRules
+{
+  public:
+    HeaderRules(){};
+
+    int apply_rules(Data const &data);
+
+}; // namespace pan
+
+}
+#endif /* HEADER-RULES_H */
