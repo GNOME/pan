5c64a419c4308a03ef5d5c2322a5cd6ce3d1dd90
Dominique Dumont <domi.dumont@free.fr>
2025-06-07 17:22:40 +0200

    my-tree: add fire_updates()
    to eventually replace fire_diffs()

--- pan/data-impl/data-impl.h
+++ pan/data-impl/data-impl.h
@@ -486,6 +486,9 @@ public:
                RulesInfo const *rules = nullptr);
         virtual ~MyTree();
 
+      protected:
+        void fire_updates() const ;
+
       public: // from ArticleTree
         void reset_article_view() const;
         void reset_article_transition_tables() const;
