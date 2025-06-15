f0a60124544a2f058188ccada7949cac82b30b79
Dominique Dumont <domi.dumont@free.fr>
2025-06-15 19:47:46 +0200

    my-tree: remove obsolete get_articles()

--- pan/data-impl/data-impl.h
+++ pan/data-impl/data-impl.h
@@ -495,7 +495,6 @@ public:
         void initialize_article_view() const override;
         void update_article_view() const override;
         Article get_parent(Quark const &mid) const override;
-        Article const *get_article(Quark const &mid) const override;
         size_t size() const override;
         void set_filter(const ShowType show_type = SHOW_ARTICLES,
                         FilterInfo const *criteria = nullptr) final override;
