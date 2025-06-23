a109fbf20086d9b7f7d389abd17913e7ff8524f8
Dominique Dumont <domi.dumont@free.fr>
2025-06-23 19:23:41 +0200

    article: manage line_count only in DB
    nzb download may be broken though: don't know what to do when
    article.clear() is called.
    
    Note:
    - line_count addition does not work if one argument is null
    - line_count is set up only when loading article part

--- pan/general/log4cxx.cc
+++ pan/general/log4cxx.cc
@@ -49,6 +49,7 @@ log4cxx::LoggerPtr getLogger(std::string const &name)
           outfile
             << "# Other loggers will come and should be listed in README.org"
             << std::endl;
+          outfile << "log4j.logger.article=WARN, A1" << std::endl;
           outfile << "log4j.logger.group=WARN, A1" << std::endl;
           outfile << "log4j.logger.group-pane=WARN, A1" << std::endl;
           outfile << "log4j.logger.header=WARN, A1" << std::endl;
