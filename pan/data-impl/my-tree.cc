abca5f7eca262ca90e2a666253a48f02a31696ab
Dominique Dumont <domi.dumont@free.fr>
2025-09-15 19:08:45 +0200

    my-tree: add call_on_hidden_articles function

--- pan/data-impl/my-tree.cc
+++ pan/data-impl/my-tree.cc
@@ -220,6 +220,26 @@ int DataImpl ::MyTree ::call_on_reparented_articles(
   return count;
 }
 
+// apply function on hidden articles
+// Returns the number of hidden articles
+int DataImpl ::MyTree ::call_on_hidden_articles(
+    std::function<void(Quark msg_id)> cb) const {
+  std::string q = R"SQL(
+    select message_id
+    from article_view as av
+    join article on av.article_id == article.id
+    where av.status = "h"
+  )SQL";
+
+  SQLite::Statement st(pan_db, q);
+  int count(0);
+  while (st.executeStep()) {
+    cb(st.getColumn(0).getText());
+    count++;
+  }
+  return count;
+}
+
 void DataImpl ::MyTree ::update_article_view() const {
   TimeElapsed timer;
   LOG4CXX_TRACE(logger, "Update article_view");
