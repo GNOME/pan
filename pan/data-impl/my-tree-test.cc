eb179f68d9b5c4583c388db1eaaa6826891132dc
Dominique Dumont <domi.dumont@free.fr>
2025-09-17 19:03:09 +0200

    article-view: test exposed articles

--- pan/data-impl/my-tree-test.cc
+++ pan/data-impl/my-tree-test.cc
@@ -516,6 +516,23 @@ class DataImplTest : public CppUnit::TestFixture
       CPPUNIT_ASSERT_EQUAL_MESSAGE("step 1 exposed count ", 10, count);
     }
 
+    void test_exposed_articles_from_scratch()
+    {
+      // init article view on empty group
+      tree =
+        data->group_get_articles("g1", "/tmp", Data::SHOW_ARTICLES, &criteria);
+      tree->initialize_article_view();
+
+      // emulate get new headers
+      add_test_articles();
+      tree->update_article_view();
+
+      // we can view the 3 read articles. They are in separate threads
+      // so they have no children
+      assert_children("step 1", Quark(), "g1", {"g1m1a", "g1m2a", "g1m2"});
+      assert_exposed("step 1", "g1m1a");
+    }
+
     // emulates showing article using a callback, and with articles
     // sorted in hierarchy
     void test_function_on_exposed_articles()
@@ -692,6 +709,7 @@ class DataImplTest : public CppUnit::TestFixture
     CPPUNIT_TEST(test_article_deletion);
     CPPUNIT_TEST(test_function_on_shown_articles);
     CPPUNIT_TEST(test_function_on_exposed_articles);
+    CPPUNIT_TEST(test_exposed_articles_from_scratch);
     CPPUNIT_TEST(test_function_on_reparented_articles);
     CPPUNIT_TEST(test_function_on_hidden_articles);
     CPPUNIT_TEST_SUITE_END();
