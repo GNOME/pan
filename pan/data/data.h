75745af11f6b4ba51a807ad2bf27ecbb57e30ee9
Dominique Dumont <domi.dumont@free.fr>
2025-09-18 18:01:04 +0200

    header-pane: fix article ordering in initial view

--- pan/data/data.h
+++ pan/data/data.h
@@ -606,7 +606,8 @@ class Data :
       virtual void update_article_view() const = 0;
       virtual void mark_as_pending_deletion(const std::set<const Article*>) const = 0;
       virtual int initial_call_on_shown_articles(
-          std::function<void(Quark msg_id, Quark parent_id)> cb) const = 0;
+          std::function<void(Quark msg_id, Quark parent_id)> cb,
+          header_column_enum sort_column, bool sort_ascending) const = 0;
       virtual int call_on_sorted_shown_articles(
           std::function<void(Quark msg_id, Quark parent_id)> cb,
           header_column_enum header_column_id = COL_DATE,
