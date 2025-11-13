e709b642bdda0b470bcf9958d31b3dd2b6e24480
Dominique Dumont <domi.dumont@free.fr>
2025-11-13 08:44:09 +0100

    ghost-article-test: improve check_ghost_presence error message
    by tracing the line where this function is called

--- pan/data-impl/ghost-article-test.cc
+++ pan/data-impl/ghost-article-test.cc
@@ -81,7 +81,7 @@ public:
     }
 
     void check_ghost_presence(std::string msg_id,
-                            bool present)
+                              bool present, int line)
     {
         SQLite::Statement query(pan_db, R"SQL(
           select count() from ghost where ghost_msg_id = ?
@@ -89,8 +89,10 @@ public:
         query.bind(1, msg_id);
 
         while(query.executeStep()) {
-            bool res = query.getColumn(0).getInt() > 0;
-            CPPUNIT_ASSERT_EQUAL_MESSAGE("ghost " + msg_id + " presence", present, res);
+          bool res = query.getColumn(0).getInt() > 0;
+          CPPUNIT_ASSERT_EQUAL_MESSAGE("ghost " + msg_id + " presence, line " +
+                                           std::to_string(line),
+                                       present, res);
         }
     }
 
@@ -178,20 +180,20 @@ public:
       // parent's reference field followed by parent's message_id,
       // with a redundant ref
       add_article(a3, a1 + " " + a2 + " " + a3);
-      check_ghost_presence(a1, true);
-      check_ghost_presence(a2, true);
+      check_ghost_presence(a1, true, __LINE__);
+      check_ghost_presence(a2, true, __LINE__);
       check_ghost_tree("a3", a3, {a3, "", a2, a1});
       check_ghost_tree("a3->a2", a2, {a2, "", a1, ""});
       check_ghost_tree("a3->a1", a1, {a1, "", "", ""});
 
       add_article(a1, "");
-      check_ghost_presence(a1, false);
-      check_ghost_presence(a2, true);
+      check_ghost_presence(a1, false, __LINE__);
+      check_ghost_presence(a2, true, __LINE__);
       check_ghost_tree("a1->a3", a3, {a3, a1, a2, a1});
       check_ghost_tree("a1->a1", a1, {a1});
 
       add_article(a2, a1);
-      check_ghost_presence(a2,false);
+      check_ghost_presence(a2,false, __LINE__);
       check_ghost_tree("a2->a3", a3, {a3, a2, "", ""});
       check_ghost_tree("a2->a2", a2, {a2, a1, "", ""});
       check_ghost_tree("a2->a1", a1, {a1});
@@ -214,29 +216,29 @@ public:
       check_ghost_tree("c3", c3, {c3, "", b2, b1});
 
       add_article(b2, b1);
-      check_ghost_presence(b2,false);
+      check_ghost_presence(b2,false, __LINE__);
       check_ghost_tree("b2->b2", b2, {b2, "", b1, ""});
       check_ghost_tree("b2->b3", b3, {b3, b2, "", ""});
       check_ghost_tree("b2->c3", c3, {c3, b2, "", ""});
 
       add_article(b1, "");
-      check_ghost_presence(b1,false);
+      check_ghost_presence(b1,false, __LINE__);
       check_ghost_tree("b1->b2", b2, {b2, b1, "", ""});
       check_ghost_tree("b1", b1, {b1});
 
       add_article(d4, b1 + " " + d2 + " " + d3);
-      check_ghost_presence(b1,false);
+      check_ghost_presence(b1,false, __LINE__);
       check_ghost_tree("d4", d4, {d4,b1,d3,d2});
       check_ghost_tree("d4->b2", b2, {b2, b1, "", ""});
       check_ghost_tree("d4->b1", b1, {b1});
       check_ghost_tree("d4->c3", c3, {c3, b2, "", ""});
 
       add_article(d3, b1 + " " + d2);
-      check_ghost_presence(d3,false);
+      check_ghost_presence(d3,false, __LINE__);
       check_ghost_tree("d3", d3, {d3,b1,d2,b1});
 
       add_article(d2, b1);
-      check_ghost_presence(d2,false);
+      check_ghost_presence(d2,false, __LINE__);
       check_ghost_tree("d2", d2, {d2,b1,"",""});
     }
 
@@ -253,7 +255,7 @@ public:
       add_article("g1m1d2", "g1m1d1 g1m1a g1m1b g1m1c2");
 
       // the above lie is discarded because g1m1a was already in DB
-      check_ghost_presence("g1m1d1", false);
+      check_ghost_presence("g1m1d1", false, __LINE__);
 
       assert_no_ghost_article();
     }
@@ -264,7 +266,7 @@ public:
 
       // add ghost ancestors and lie about one ancestor
       add_article("g1m1d2", "g1m1d1 g1m1a g1m1b g1m1c2");
-      check_ghost_presence("g1m1d1", true);
+      check_ghost_presence("g1m1d1", true, __LINE__);
 
       add_article("g1m1a", ""); // clobber g1m1a ghost article
 
@@ -274,7 +276,7 @@ public:
       add_article("g1m1d1", "g1m1a g1m1b g1m1c1");
 
       // the lie is discarded because g1m1a was already in DB
-      check_ghost_presence("g1m1d1", false);
+      check_ghost_presence("g1m1d1", false, __LINE__);
 
       // check that ghost articles are gone
       assert_no_ghost_article();
@@ -303,34 +305,34 @@ public:
       // b1 -> d2 ->(d3)-> d4 -> d5 -> d6
       //   \-> b2 -> c3
       //         \-> b3
-      check_ghost_presence(d3, true);
+      check_ghost_presence(d3, true, __LINE__);
       check_ghost_tree("del d3->d4", d4, {d4,d2,d3,d2});
 
       delete_article(d2);
       // b1 ->(d2)->(d3)-> d4 -> d5 -> d6
       //   \-> b2 -> c3
       //         \-> b3
-      check_ghost_presence(d2, true);
-      check_ghost_presence(d3, true);
+      check_ghost_presence(d2, true, __LINE__);
+      check_ghost_presence(d3, true, __LINE__);
       check_ghost_tree("del d2->d4", d4, {d4,b1,d3,d2});
 
       delete_article(d4);
       // b1 ->(d2)->(d3)->(d4)-> d5 -> d6
       //   \-> b2 -> c3
       //         \-> b3
-      check_ghost_presence(d2, true);
-      check_ghost_presence(d3, true);
-      check_ghost_presence(d4, true);
+      check_ghost_presence(d2, true, __LINE__);
+      check_ghost_presence(d3, true, __LINE__);
+      check_ghost_presence(d4, true, __LINE__);
       check_ghost_tree("del d4->d5", d5, {d5,b1,d4,d3});
 
       delete_article(b2);
       // b1 ->(d2)->(d3)->(d4)-> d5 -> d6
       //   \->(b2)-> c3
       //         \-> b3
-      check_ghost_presence(b2, true);
-      check_ghost_presence(d2, true);
-      check_ghost_presence(d3, true);
-      check_ghost_presence(d4, true);
+      check_ghost_presence(b2, true, __LINE__);
+      check_ghost_presence(d2, true, __LINE__);
+      check_ghost_presence(d3, true, __LINE__);
+      check_ghost_presence(d4, true, __LINE__);
       check_ghost_tree("del b2->c3", c3, {c3,b1,b2,b1});
       check_ghost_tree("del b2->b3", b3, {b3,b1,b2,b1});
 
@@ -338,11 +340,11 @@ public:
       //(b1)->(d2)->(d3)->(d4)-> d5 -> d6
       //   \->(b2)-> c3
       //         \-> b3
-      check_ghost_presence(b1, true);
-      check_ghost_presence(b2, true);
-      check_ghost_presence(d2, true);
-      check_ghost_presence(d3, true);
-      check_ghost_presence(d4, true);
+      check_ghost_presence(b1, true, __LINE__);
+      check_ghost_presence(b2, true, __LINE__);
+      check_ghost_presence(d2, true, __LINE__);
+      check_ghost_presence(d3, true, __LINE__);
+      check_ghost_presence(d4, true, __LINE__);
       check_ghost_tree("del b1->c3", c3, {c3,"",b2,b1});
       check_ghost_tree("del b1->b3", b3, {b3,"",b2,b1});
 
@@ -351,30 +353,30 @@ public:
       //(b1)->(d2)->(d3)->(d4)-> d5 -> d6
       //   \->(b2)
       //         \-> b3
-      check_ghost_presence(c3, false);
-      check_ghost_presence(b1, true);
-      check_ghost_presence(b2, true);
+      check_ghost_presence(c3, false, __LINE__);
+      check_ghost_presence(b1, true, __LINE__);
+      check_ghost_presence(b2, true, __LINE__);
 
       // triggers a complete deletion of b3 and b2 since there's no
       // other child
       //(b1)->(d2)->(d3)->(d4)-> d5 -> d6
       delete_article(b3);
-      check_ghost_presence(b3, false);
-      check_ghost_presence(b2, false);
-      check_ghost_presence(b1, true);
+      check_ghost_presence(b3, false, __LINE__);
+      check_ghost_presence(b2, false, __LINE__);
+      check_ghost_presence(b1, true, __LINE__);
 
       delete_article(d6);
       //(b1)->(d2)->(d3)->(d4)-> d5
-      check_ghost_presence(d6, false);
-      check_ghost_presence(d5, false);
+      check_ghost_presence(d6, false, __LINE__);
+      check_ghost_presence(d5, false, __LINE__);
 
       // delete last article which should suppress remaining ghosts
       delete_article(d5);
-      check_ghost_presence(d5, false);
-      check_ghost_presence(d4, false);
-      check_ghost_presence(d3, false);
-      check_ghost_presence(d2, false);
-      check_ghost_presence(b1, false);
+      check_ghost_presence(d5, false, __LINE__);
+      check_ghost_presence(d4, false, __LINE__);
+      check_ghost_presence(d3, false, __LINE__);
+      check_ghost_presence(d2, false, __LINE__);
+      check_ghost_presence(b1, false, __LINE__);
     }
 
     CPPUNIT_TEST_SUITE(DataImplTest);
